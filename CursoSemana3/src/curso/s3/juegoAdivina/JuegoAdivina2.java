a3a6e8d53c197f7700287ab2e7759d145d12d343
Yuqian <rosanahu97@gmail.com>
2023-01-31 15:31:20 +0100

    Ejercicio adivina corregido

--- CursoSemana3/src/curso/s3/juegoAdivina/JuegoAdivina2.java
+++ CursoSemana3/src/curso/s3/juegoAdivina/JuegoAdivina2.java
@@ -10,47 +10,57 @@ public class JuegoAdivina2 {
 		static int topeSup =1000;
 		static int topeInf =0;
 		static int nMaquina;
+		static int rMaquina;
+		static Scanner sc;
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		 sc= new Scanner(System.in);
 		Random rn = new Random();
 		nMaquina = rn.nextInt(topeSup);
-		int rMaquina ;
+		rMaquina =getRandomNumberInRange(topeInf,topeSup);			
 		//mientras el numero introducido no es correcto, se repite el bucle
-			while(!finJuego) {
+			
+		while(!finJuego) {
+					juegaMaquina();
+					
+				if(!finPartido && !finJuego) {
+					juegoPartido();
+				}
+		}
 
-				rMaquina =getRandomNumberInRange(topeInf,topeSup);			
+	}
+	private static void juegaMaquina() {
 		System.out.println("Numero: "+ rMaquina);
-		System.out.println("M=Mayor, m = Menor, f= Fin");
+		System.out.println("M=El numero correcto es mayor, m = El numero correcto es  menor, f= Fin");
 				String miRespuesta = sc.next();
 				switch(miRespuesta){		
-				case "M": rMaquina =getRandomNumberInRange(topeInf, rMaquina);break;
-				case "m": rMaquina =getRandomNumberInRange(rMaquina,topeSup);break;
+				case "M": 	topeInf = rMaquina;
+							rMaquina =getRandomNumberInRange(topeInf,topeSup);break;
+				case "m": 	topeSup = rMaquina;
+							rMaquina =getRandomNumberInRange(topeInf,topeSup);break;
 				case "f": finPartido = true;
 						  finJuego = true;
 							System.out.println("Numero correcto!!!");	break;
-				}			
-				if(!finPartido) {
+				}		
+	}
+	private static void juegoPartido() {
 		System.out.println("Introduce un numero:");
-				miNumero = sc.nextInt();
-				if(miNumero==nMaquina) {
-					System.out.println("Numero correcto!!!");	
-					//sale del bucle
-					finJuego = true;
-				}else if(miNumero>nMaquina) {
-				System.out.println("El numero correcto es menor que "+miNumero);
+		miNumero = sc.nextInt();
+		if(miNumero==nMaquina) {
+			System.out.println("Numero correcto!!!");	
+			//sale del bucle
+			finJuego = true;
+		}else if(miNumero>nMaquina) {
+		System.out.println("El numero correcto es menor que "+miNumero);
 
-				}else {
-				System.out.println("El numero correcto es mayor que "+miNumero);
-
-				}
-				}
+		}else {
+		System.out.println("El numero correcto es mayor que "+miNumero);
 
 		}
-
-
 	}
+	
+	
 	private static int getRandomNumberInRange(int min, int max) {
-
+		//System.out.println("valor min y max:"+min+" "+max);
 		if (min >= max) {
 			throw new IllegalArgumentException("max must be greater than min");
 		}
